65289910cad79cf6e98e4099d9c1dacad52d4590
吉川学 <>
2023-05-08 00:14:41 +0900

    bladeファイルを読み込み通常のphpファイルに変換する処理の調整(ディレクトリ一部変更、ソースの見やすさ調整)

--- functions.php
+++ functions.php
@@ -1,6 +1,13 @@
 <?php
 
-require_once(__DIR__ . '/vendor/autoload.php');
+/**
+ * wordpressの命名規則では変数・関数はスネークケース(全て小文字)にする
+ * 参考：https://www.sejuku.net/blog/104248
+ */
+
+// このメソッドが呼び出されたファイル(functions.php)があるディレクトリのフルパスを取得
+$dir = __DIR__;
+require_once("{$dir}/vendor/autoload.php");
 
 use Jenssegers\Blade\Blade;
 
@@ -10,8 +17,12 @@ use Jenssegers\Blade\Blade;
 if (!function_exists('render_blade')) {
     function render_blade($template_name)
     {
-        $blade = new Blade(__DIR__ . '/views/pages', __DIR__ . '/cache');
-
-        return $blade->make($template_name);
+        /**
+         * グローバル変数を呼び出し
+         * 参考：https://webukatu.com/wordpress/blog/28688/
+         */
+        global $dir;
+        $blade = new Blade("{$dir}/views", "{$dir}/cache");
+        return $blade->make("pages/{$template_name}");
     }
 }
